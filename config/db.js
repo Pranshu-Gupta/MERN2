55cd5031deef169d070491280fc4cc951d1ed966
pranshu gupta <pranshugupta2910@gmail.com>
2019-12-15 04:32:03 +0530

    Users.js

---
+++ config/db.js
@@ -0,0 +1,22 @@
+ const mongoose = require('mongoose');
+ const config = require('config');
+ const db = config.get('mongoURI');
+
+ const connectDB = async () => {
+     try {
+
+        //As it is going to return a promise, we must have an await statement, we can either use fetch and catch or this.
+
+        await mongoose.connect(db,{ useNewUrlParser: true ,useUnifiedTopology: true});
+
+        console.log('MongoDB connected..');
+
+     } catch (error) {
+
+        console.error(error.message);
+        //Exit
+        process.exit(1);
+     }
+ }
+
+ module.exports = connectDB;
